65c6595f58e43a87e14847335f0b8a2ddc573f48
Abhijit <444abhiupasani@gmail.com>
2025-08-14 00:40:53 +0530

    CRUD library

---
+++ libraryproject/books/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render,redirect, get_object_or_404
+from .models import Book
+from django import forms
+# Create your views here.
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
+        
+    
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'books/book_list.html', {'books': books})
+
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/book_form.html', {'form': form})
+        
+def book_update(request, id):
+    book = get_object_or_404(Book, id=id)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/book_form.html', {'form': form})
+
+def book_delete(request, id):
+    book = get_object_or_404(Book, id=id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')
+    else:
+        return render(request, 'books/book_confirm_delete.html', {'book': book})
+    
+            
