7da81b9c79389024b1a1d74046936d61f727c776
Abhijit <444abhiupasani@gmail.com>
2025-08-15 16:55:42 +0530

    Serializers and Viewset

--- libraryproject/books/urls.py
+++ libraryproject/books/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
-from . import views
+from django.urls import include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+# urlpatterns = [
+#     path('', views.book_list, name='book_list'),
+#     path('add/', views.add_book, name='book_create'),
+#     path('edit/<int:pk>/', views.book_update, name='book_update'),
+#     path('delete/<int:pk>/', views.book_delete, name='book_delete'),
+#     path('api/books/', views.book_list_api, name='book_list_api'),
+#     path('api/books/<int:id>/', views.book_detail_api, name='book_detail_api'),
+#     path('api/books/create/', views.book_create_api, name='book_create_api'),
+#     path('api/books/<int:id>/update/', views.book_update_api, name='book_update_api'),
+#     path('api/books/<int:id>/delete/', views.book_delete_api, name='book_delete_api'),
+
+# ]
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
 
 urlpatterns = [
-    path('', views.book_list, name='book_list'),
-    path('add/', views.add_book, name='book_create'),
-    path('edit/<int:pk>/', views.book_update, name='book_update'),
-    path('delete/<int:pk>/', views.book_delete, name='book_delete'),
-    path('api/books/', views.book_list_api, name='book_list_api'),
-    path('api/books/<int:id>/', views.book_detail_api, name='book_detail_api'),
-    path('api/books/create/', views.book_create_api, name='book_create_api'),
-    path('api/books/<int:id>/update/', views.book_update_api, name='book_update_api'),
-    path('api/books/<int:id>/delete/', views.book_delete_api, name='book_delete_api'),
-
+    path('api/', include(router.urls)),
 ]
--- libraryproject/books/views.py
+++ libraryproject/books/views.py
@@ -1,3 +1,5 @@
+from rest_framework import viewsets
+from .serializers import BookSerializer
 from django.shortcuts import render,redirect, get_object_or_404
 from django.http import JsonResponse
 from .models import Book
@@ -57,80 +59,85 @@ def add_book(request):
     
     return render(request, 'books/book_form.html', {'form': form})
 
-def book_list_api(request):
-    books = Book.objects.all().values('id', 'title', 'author', 'published_date')
-    return JsonResponse(list(books), safe=False)
+# def book_list_api(request):
+#     books = Book.objects.all().values('id', 'title', 'author', 'published_date')
+#     return JsonResponse(list(books), safe=False)
 
-def book_detail_api(request, id):
-    book = get_object_or_404(Book, id=id)
-    data = {
-        'id': book.id,
-        'title': book.title,
-        'author':book.author,
-        'published_date': book.published_date,
-    }
-    return JsonResponse(data, safe=False)
+# def book_detail_api(request, id):
+#     book = get_object_or_404(Book, id=id)
+#     data = {
+#         'id': book.id,
+#         'title': book.title,
+#         'author':book.author,
+#         'published_date': book.published_date,
+#     }
+#     return JsonResponse(data, safe=False)
 
-@csrf_exempt  # only for testing — not safe for production
-def book_create_api(request):
-    if request.method =='POST':
-        try:
-            data = json.loads(request.body)
+# @csrf_exempt  # only for testing — not safe for production
+# def book_create_api(request):
+#     if request.method =='POST':
+#         try:
+#             data = json.loads(request.body)
             
-            book = Book.objects.create(
-                title = data.get('title'),
-                author = data.get('author'),
-                published_date = data.get('published_date') #YYYY-MM-DD format
-            )
-            return JsonResponse({
-                "title": book.title,
-                "author": book.author,
-                "published_date": book.published_date
-            }, status=201)
-        except json.JSONDecodeError:
-            return JsonResponse({"error": "Invalid JSON"}, status=400)
+#             book = Book.objects.create(
+#                 title = data.get('title'),
+#                 author = data.get('author'),
+#                 published_date = data.get('published_date') #YYYY-MM-DD format
+#             )
+#             return JsonResponse({
+#                 "title": book.title,
+#                 "author": book.author,
+#                 "published_date": book.published_date
+#             }, status=201)
+#         except json.JSONDecodeError:
+#             return JsonResponse({"error": "Invalid JSON"}, status=400)
     
-    return JsonResponse({"error": "Only POST method is allowed"}, status=405)
+#     return JsonResponse({"error": "Only POST method is allowed"}, status=405)
 
 
-@csrf_exempt
-def book_update_api(request, id):
-    if request.method == 'PUT':
-        try:
-            book = Book.objects.get(id=id)
-        except:
-            return JsonResponse({"error": "Book does not exist"}, status=404)
+# @csrf_exempt
+# def book_update_api(request, id):
+#     if request.method == 'PUT':
+#         try:
+#             book = Book.objects.get(id=id)
+#         except:
+#             return JsonResponse({"error": "Book does not exist"}, status=404)
         
-        try:
-            data = json.loads(request.body)
-        except json.JSONDecodeError:
-            return JsonResponse({"error": "Invalid JSON"}, status=400)
+#         try:
+#             data = json.loads(request.body)
+#         except json.JSONDecodeError:
+#             return JsonResponse({"error": "Invalid JSON"}, status=400)
         
-        if "title" in data:
-            book.title = data["title"]
-        if "author" in data:
-            book.author = data["author"]
-        if "published_date" in data:
-            book.published_date = data["published_date"]
-        book.save()
+#         if "title" in data:
+#             book.title = data["title"]
+#         if "author" in data:
+#             book.author = data["author"]
+#         if "published_date" in data:
+#             book.published_date = data["published_date"]
+#         book.save()
         
-        return JsonResponse({
-            "id": book.id,
-            "title": book.title,
-            "author": book.author,
-            "published_date": book.published_date
-        })
+#         return JsonResponse({
+#             "id": book.id,
+#             "title": book.title,
+#             "author": book.author,
+#             "published_date": book.published_date
+#         })
         
-    return JsonResponse({"error": "Only PUT method is allowed"}, status=405)
+#     return JsonResponse({"error": "Only PUT method is allowed"}, status=405)
 
-@csrf_exempt
-def book_delete_api(request, id):
-    if request.method == 'DELETE':
-        try:
-            book = Book.objects.get(id=id)
-        except Book.DoesNotExist:
-            return JsonResponse({"error": "Book does not exist"}, status=404)
+# @csrf_exempt
+# def book_delete_api(request, id):
+#     if request.method == 'DELETE':
+#         try:
+#             book = Book.objects.get(id=id)
+#         except Book.DoesNotExist:
+#             return JsonResponse({"error": "Book does not exist"}, status=404)
         
-        book.delete()
-        return JsonResponse({"message": "Book deleted successfully"}, status=204)
-    return JsonResponse({"error": "Only DELETE method is allowed"}, status=405)    
+#         book.delete()
+#         return JsonResponse({"message": "Book deleted successfully"}, status=204)
+#     return JsonResponse({"error": "Only DELETE method is allowed"}, status=405)    
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
