ed4e918b1d005e8b4dc03eb507869ccb8b774a4e
Abhijit <444abhiupasani@gmail.com>
2025-08-16 00:20:54 +0530

    Created By User specific

--- libraryproject/books/models.py
+++ libraryproject/books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,6 +8,7 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     published_date = models.DateField()
     cover = models.ImageField(upload_to='covers/', blank=True, null=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     
     def __str__(self):
         return self.title
